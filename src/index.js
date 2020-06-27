4b7e0ce7ff3c1809f3a598cee6582a00131be4bd
Roman Zhydyk <roman@student.dei.uc.pt>
2020-06-27 16:05:04 +0100

    Added Authentication Middleware to User

--- src/index.js
+++ src/index.js
@@ -7,22 +7,14 @@ const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 3000
 
-//MIDDLEWAREs (allows to enforce change without needing to look at multiple places)
-app
+//MIDDLEWAREs (allows to run code between req and res)
+//Without Middleware: new req -> run route handler
+//With Middleware: new req -> do something -> run route handler
+app 
     .use(express.json())
     .use(userRouter)
     .use(taskRouter)
 
-    .listen(port, () => {
-        console.log('Server is up on p ort ' + port)
-    })
-
-/* const myFunc = async () => {
-    const token = jwt.sign({_id: 'abc123'}, 'cat')
-    console.log(token)
-
-    const data = jwt.verify(token, 'cat')
-    console.log(data)
-}
-
-myFunc() */
+app.listen(port, () => {
+    console.log('Server is up on p ort ' + port)
+})
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 const router = new express.Router()
 
@@ -14,7 +15,6 @@ router
                 user,
                 token
             })
-            console.log('signed up')
         } catch (error) {
             res.status(400).send(error)
         }
@@ -23,23 +23,16 @@ router
         try {
             const user = await User.findByCredentials(req.body.email, req.body.password)
             const token = await user.generateAuthToken()
-
             res.send({
                 user,
                 token
             })
-            console.log('logged in')
         } catch (error) {
             res.status(400).send()
         }
     })
-    .get('/users', async (req, res) => {
-        try {
-            const users = await User.find({})
-            res.send(users)
-        } catch (error) {
-            res.status(500).send(error)
-        }
+    .get('/users/me', auth, async (req, res) => {
+        res.send(req.user)
     })
     .get('/users/:id', async (req, res) => {
         const _id = req.params.id
@@ -66,12 +59,7 @@ router
 
         try {
             const user = await User.findById(_id)
-            
-            // DOESN'T TRIGGER THE SAVE MIDDLEWARE!
-            /* const user = await User.findByIdAndUpdate(_id, body, {
-                new: true,
-                runValidators: true
-            }) */
+
             if (!user) return res.status(404).send()
 
             updates.forEach((update) => user[update] = body[update])
