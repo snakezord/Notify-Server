fe7fb05a7aeb34784a4b792a649d778c0788d9da
Roman Zhydyk <roman@student.dei.uc.pt>
2020-12-06 14:19:57 +0000

    Revert "added proxy middleware for CORS"
    This reverts commit efcfd8c2c97d40aae5cfbafba642a225f24e5131.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const avatarRouter = require('./routers/avatar');
@@ -11,7 +10,6 @@ const app = express()
 //Without Middleware: new req -> run route handler
 //With Middleware: new req -> do something -> run route handler
 app
-    .use('/', createProxyMiddleware({ target: 'https://roman-task-app.herokuapp.com', changeOrigin: true }))
     .use(express.json())
     .use(userRouter)
     .use(avatarRouter)
