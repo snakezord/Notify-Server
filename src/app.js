e67a8a82adc0ec022b0b187fc7c996c3fb5f9a93
Roman Zhydyk <roman@student.dei.uc.pt>
2020-12-30 18:02:21 +0000

    trying to fix 405 on client

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('./db/mongoose');
+var cors = require('cors')
 const userRouter = require('./routers/user');
 const avatarRouter = require('./routers/avatar');
 const taskRouter = require('./routers/task');
@@ -11,6 +12,7 @@ const app = express()
 //With Middleware: new req -> do something -> run route handler
 app
     .use(express.json())
+    .use(cors())
     .use(userRouter)
     .use(avatarRouter)
     .use(taskRouter)
